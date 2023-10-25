c7c91df8b828f79b87470fbec3b18fc18a26b442
stackasteak <stackasteak@gmail.com>
2023-10-25 13:58:44 -0700

    files

---
+++ script.js
@@ -0,0 +1,16 @@
+const canvas = document.querySelector('.myCanvas');
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight-85;
+const ctx = canvas.getContext('2d');
+
+
+
+
+const xPicker = document.querySelector('rangex');
+const yPicker = document.querySelector('rangey');
+const outputx = document.querySelector('outputx');
+const outputy = document.querySelector('outputy');
+const backBtn = document.querySelector('backBtn');
+const forwBtn = document.querySelector('forwBtn');
+const clearBtn = document.querySelector('clearBtn');
+
