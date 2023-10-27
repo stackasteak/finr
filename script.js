8fe955f468b699dbaf84a648f4b006d22b4784e0
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-10-27 05:59:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,32 @@ const backBtn = document.querySelector('backBtn');
 const forwBtn = document.querySelector('forwBtn');
 const clearBtn = document.querySelector('clearBtn');
 
+// update sizepicker output value
+
+xPicker.addEventListener('input', () => outputx.textContent = xPicker.value);
+
+yPicker.addEventListener('input', () => outputy.textContent = yPicker.value);
+
+// store mouse pointer coordinates, and whether the button is pressed
+let curX;
+let curY;
+let pressed = false;
+
+// update mouse pointer coordinates
+document.addEventListener('mousemove', e => {
+  curX = (window.Event) ? e.pageX : e.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+  curY = (window.Event) ? e.pageY : e.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+});
+
+canvas.addEventListener('mousedown', () => pressed = true);
+
+canvas.addEventListener('mouseup', () => pressed = false);
+
+canvas.addEventListener('touchstart', (e) => {
+  pressed = true;
+  curX = (window.Event) ? e.pageX : e.touches[0].clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+  curY = (window.Event) ? e.pageY : e.touches[0].clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+}
+);
+
+canvas.addEventListener('touchend', () => pressed = false);
