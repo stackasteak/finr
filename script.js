ecfefcd3ea49676a8742ae22089ccb30e34fa7ad
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-10-27 06:15:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const ctx = canvas.getContext('2d');
 
 
 
-const xPicker = document.querySelector('rangex');
-const yPicker = document.querySelector('rangey');
-const outputx = document.querySelector('outputx');
-const outputy = document.querySelector('outputy');
-const backBtn = document.querySelector('backBtn');
-const forwBtn = document.querySelector('forwBtn');
-const clearBtn = document.querySelector('clearBtn');
+const xPicker = document.querySelector('.rangex');
+const yPicker = document.querySelector('.rangey');
+const outputx = document.querySelector('.outputx');
+const outputy = document.querySelector('.outputy');
+const backBtn = document.querySelector('.backBtn');
+const forwBtn = document.querySelector('.forwBtn');
+const clearBtn = document.querySelector('.clearBtn');
 
 // update sizepicker output value
 
