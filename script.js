64074ee36cffd4189d8e06e8f5390bcaaaa37a65
stackasteak <stackasteak@gmail.com>
2023-10-29 17:25:02 -0700

    a

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ const backBtn = document.querySelector('.backBtn');
 const forwBtn = document.querySelector('.forwBtn');
 const clearBtn = document.querySelector('.clearBtn');
 
+//drawing variables
+let stonesize;
+let xoffset=0;
+
+// store mouse pointer coordinates, and whether the button is pressed
+let curX;
+let curY;
+let pressed = false;
+
 const boardstate = {
   xsize: 9,
   ysize: 9,
@@ -23,20 +32,16 @@ function redraw(){
   ctx.fillStyle = 'rgb(0,0,255)';
   ctx.fillRect(0,0,width,height);
 
-  
-
-  let stonesize;
-  let xoffset=0;
-  if(xsize>=ysize){
-    stonesize = width/xsize;
+  if(boardstate.xsize>=boardstate.ysize){
+    stonesize = width/boardstate.xsize;
   }
   else{
-    stonesize = width/ysize;
-    xoffset = (width-stonesize*xsize)*0.5;
+    stonesize = width/boardstate.ysize;
+    xoffset = (width-stonesize*boardstate.xsize)*0.5;
   }
 
-  for(let ii=0; ii<xsize; ii++){
-    for(let jj=0; jj<ysize; jj++){
+  for(let ii=0; ii<boardstate.xsize; ii++){
+    for(let jj=0; jj<boardstate.ysize; jj++){
       ctx.fillStyle = "rgb(0, 0, 0)";
       ctx.beginPath();
       ctx.arc(xoffset+(ii+0.5)*stonesize,(jj+0.5)*stonesize , 0.5*stonesize-2, degToRad(0), degToRad(360), false);
@@ -45,37 +50,51 @@ function redraw(){
   }
 
   ctx.fillStyle = 'rgb(0,0,0)'; 
-  ctx.fillRect(0, stonesize*ysize, width, height);
+  ctx.fillRect(0, stonesize*boardstate.ysize, width, height);
 }
 
 function setSize(){
   if(sizePicker.value =="6x7"){
-    xsize = 7;
-    ysize = 6;
+    xboardstate.size = 7;
+    yboardstate.size = 6;
   }
   if(sizePicker.value =="8x8"){
-    xsize = 8;
-    ysize = 8;
+    xboardstate.size = 8;
+    yboardstate.size = 8;
   }
   if(sizePicker.value =="9x9"){
-    xsize = 9;
-    ysize = 9;
+    xboardstate.size = 9;
+    yboardstate.size = 9;
   }
   if(sizePicker.value =="10x10"){
-    xsize = 10;
-    ysize = 10;
+    xboardstate.size = 10;
+    yboardstate.size = 10;
   }
 }
+
+function drawhighlight(){
+  let iselect;
+  for(let ii=0; ii<boardstate.xsize; ii++){
+    if(curX < xoffset+(ii+1)*stonesize){
+      iselect = ii;
+      break;
+    }
+  }
+  for(let jj=boardstate.nextys[iselect]; jj<boardstate.ysize; jj++){
+    ctx.fillStyle = "rgb(0, 0, 0)";
+    ctx.beginPath();
+    ctx.arc(xoffset+(iselect+0.5)*stonesize,(jj+0.5)*stonesize , 0.5*stonesize-2, degToRad(0), degToRad(360), false);
+    ctx.fill();
+  }
+}
+
+function undrawhighlight()
+
+
 // update sizepicker output value
 
-//document.getElementbyId("bsize").onchange = redraw;
 sizePicker.onchange = ( () => {setSize(); redraw();} );
 
-
-// store mouse pointer coordinates, and whether the button is pressed
-let curX;
-let curY;
-let pressed = false;
 
 // update mouse pointer coordinates
 document.addEventListener('mousemove', e => {
@@ -91,10 +110,16 @@ canvas.addEventListener('touchstart', (e) => {
   pressed = true;
   curX = (window.Event) ? e.pageX : e.touches[0].clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
   curY = (window.Event) ? e.pageY : e.touches[0].clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+  drawhighlight();
 }
 );
 
-canvas.addEventListener('touchend', () => pressed = false);
+canvas.addEventListener('touchend', (e) => {
+  pressed = false;
+  curX = (window.Event) ? e.pageX : e.touches[0].clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+  curY = (window.Event) ? e.pageY : e.touches[0].clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+  drawmove();
+});
 
 clearBtn.addEventListener('click', () => {
   redraw();
