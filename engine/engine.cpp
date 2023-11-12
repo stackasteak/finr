12a05dfdc80263539d0ba3c5239823691a0c8cff
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-11-12 07:20:08 -0800

    Update engine.cpp

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1,2 +1,12 @@
 #include <emscripten/bind.h>
 #include "Game.h"
+#include <vector.h>
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(module) {
+
+  // register bindings for std::vector<int> and std::map<int, std::string>.
+  register_vector<float>("vector<float>");
+
+}
