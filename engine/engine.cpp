fec54bca5b8e762356ec0ff1c985d0f503549c83
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-11-12 07:56:00 -0800

    Update engine.cpp

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -4,7 +4,19 @@
 
 using namespace emscripten;
 
-EMSCRIPTEN_BINDINGS(module) {
+EMSCRIPTEN_BINDINGS(my_class_example) {
+  class_<MyClass>("MyClass")
+    .constructor<int, std::string>()
+    .function("incrementX", &MyClass::incrementX)
+    .property("x", &MyClass::getX, &MyClass::setX)
+    .property("x_readonly", &MyClass::getX)
+    .class_function("getStringFromInstance", &MyClass::getStringFromInstance)
+    ;
+}
+
+
+
+EMSCRIPTEN_BINDINGS(engine_module) {
 
   // register bindings for std::vector<int> and std::map<int, std::string>.
   register_vector<float>("vector<float>");
