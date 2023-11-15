28082652938e06f3e5266034570c997c8e4c98c2
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-11-15 12:09:19 -0800

    Update engine_worker.js

--- engine_worker.js
+++ engine_worker.js
@@ -23,6 +23,7 @@ function doBatch(){
 onmessage = (e) => {
   if (e.data.msg == "start"){
     //estate.resize(e.data.bs.xsize,e.data.bs.ysize);
+    estate = new Module.engineState(9,9);
     estate.resize0();
     //doBatch();
     postMessage([9]);
