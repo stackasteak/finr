ae91415a4b078cfa786fd8757bad946a128bda10
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-11-14 23:01:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -200,6 +200,7 @@ function undrawmove(ii){
 }
 
 function drawP(p){
+  ctx.fillStyle = "rgb(255,255,255)";
   for(let ii=0; ii<boardstate.xsize; ii++){
     let jj=boardstate.nextys[ii];
     if (jj<boardstate.ysize){
