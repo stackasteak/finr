0754e557d8f7283b931eea76ab298d166eda3f62
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-11-14 22:26:16 -0800

    Update engine_worker.js

--- engine_worker.js
+++ engine_worker.js
@@ -2,13 +2,25 @@ importScripts("engine/engine.js");
 
 var estate = new Module.engineState(9,9); 
 
+var timeoutID;
 function doBatch(){
   estate.batch();
-  postMessage({msg
+  let p =[];
+  for( let ii=0; ii<estate.getNX(); ii++){
+    p.push(estate.getP(ii));
+  }
+  postMessage(p);
+  timeoutID = setTimeout(doBatch,2000);
+}
 
 onmessage = (e) => {
   if (e.data.msg == "start"){
     estate.resize(e.data.bs.xsize,e.data.bs.ysize);
+    doBatch();
+  }
+
+  if (e.data.msg == "stop"){
+    clearTimeout(timeoutID);
   }
     
 };
