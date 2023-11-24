d84b2f5fc65dddaedd5b6b80286d47b7ceb65e59
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-11-23 20:54:58 -0800

    Update engine.cpp

--- engine.cpp
+++ engine.cpp
@@ -403,9 +403,10 @@ EM_JS(void, undrawmove, (float xpos, float ypos, float ss), {
 });
 
 EM_JS(int, loadhiststep, (int nx, int ny), {
-  let re = /[^\d]*(\d+)[^\d]* /g
+  let re = /[^\d]*(\d+)[^\d]*/g ;
+  let h = Module.pastehistBox.value;
   let m = [...h.matchAll(re)];
-  return m.map((mm) => Number(mm[1]));
+  let moves = m.map((mm) => Number(mm[1]));
 });
 
 
