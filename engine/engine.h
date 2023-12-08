5157542c66703bb1b8d049a0f40a8b3418f44e16
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-12-08 07:26:42 -0800

    Update engine.h

--- engine/engine.h
+++ engine/engine.h
@@ -1,12 +1,13 @@
 #include <vector>
 
-struct baseEngine{
+
+struct engineface{
   std::vector<float> p;
   int nx;
   int ny;
   bool running;
 
-  baseEngine(int nx_, int ny_) : nx(nx_), ny(ny_){
+  baseEngine(int nx_, int ny_, runType rr_) : nx(nx_), ny(ny_), rr(rr_){
     p.resize(nx_,0.0);
     running = false;
   }
@@ -17,19 +18,15 @@ struct baseEngine{
     p.resize(nx_,0.0);
   }
 
-  void run(){}
-
 
 };
 
 
-struct randEngine : public baseEngine{
-
-  randEngine(int nx_, int ny_) : baseEngine(nx,ny) {};
+struct runrand {
  
-  void run(){
-    for(int ii=0; ii <nx; ii++){
-       p[ii]= (std::rand()%100)/100.0;
+  void run(engineface & ef){
+    for(int ii=0; ii <ef.nx; ii++){
+       ef.p[ii]= (std::rand()%100)/100.0;
      }
   }
 };
