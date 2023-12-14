4aa5ca501fa087e59ffdc3be97da983ee47209f5
stackasteak <146687315+stackasteak@users.noreply.github.com>
2023-12-14 08:40:26 -0800

    Update engine.h

--- engine/engine.h
+++ engine/engine.h
@@ -84,14 +84,14 @@ template<class allStateType>
 struct runrand1 {
 
   Game gg;
-  int ncount;
   int maxcount;
+  int ncount_tot;
   int nx;
   int ny;
 
   runrand1(int nx_, int ny_) : nx(nx_), ny(ny_){
     gg.reset();
-    ncount =0;
+    ncount_tot =0;
     maxcount=100;
   }
  
@@ -104,57 +104,20 @@ struct runrand1 {
       }
     }
     
-    for(int ii=0;ii<maxcount; ii++){
-      //set up position
-      gg.reset();
-      for(int imove=0; imove< asp->dsp->history.size(); imove++){
-        gg.makemove(asp->dsp->history[imove]);
-      }
-
-      //play moves
-      int inimove=-1;
-      for(int imove=0; imove<nx*ny; imove++){
-        
-        std::vector<int> plms;
-        for(int kk=0; kk<nx; kk++){
-          if(gg.isplayable(kk)){
-            plms.push_back(kk);
-          }
-        }
-        //int kk1= std::rand()%plms.size();
-        int kk1= int(floor(emscripten_random() * plms.size()));
-        int kk2= plms[kk1];
-        gg.makemove(kk2);
-        if (imove==0) inimove=kk2;
-        /*
-        while(true){
-          jj= std::rand()%nx;
-          if(gg.isplayable(jj)){
-            break;
-          }
-        }
-        */
-        //gg.makemove(jj);
-        
-        if(gg.nplies==nx*ny){
-          
-          asp->esp->p[inimove] = asp->esp->p[inimove] * (ncount/(ncount+1.0)) + 0.5/(ncount+1);
-          ncount++;
-          break;
-        }
-        else if(gg.haswon(gg.color[0])){
-          
-          asp->esp->p[inimove] = asp->esp->p[inimove] * (ncount/(ncount+1.0)) + 1.0/(ncount+1);
-          ncount++;
-          break;
-        }
-        else if(gg.haswon(gg.color[1])){
-          
-          asp->esp->p[inimove] = asp->esp->p[inimove] * (ncount/(ncount+1.0)) + 0.0/(ncount+1);
-          ncount++;
-          break;
-        }
-      }
+    //set up position
+    gg.reset();
+    for(int imove=0; imove< asp->dsp->history.size(); imove++){
+      gg.makemove(asp->dsp->history[imove]);
     }
+
+    auto res = randrollout_policy(gg,nx,ny,maxcount);
+    int nn = std::get<0>(res);
+    std:vector<float> ptemp = std::get<1>(res);
+    
+    for(int jj=0; jj<nx, jj++){
+      asp->esp->p[jj] = asp->esp->p[jj] * (ncount_tot/(ncount_tot+nn)) + ptemp[jj] * nn/(ncount_tot+nn);
+    }
+    ncount_tot = ncount_tot + nn;
+    
   }
 };
