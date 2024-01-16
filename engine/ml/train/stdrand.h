6dfa37718753c95327f84c5878c259cc3b402910
stackasteak <146687315+stackasteak@users.noreply.github.com>
2024-01-16 13:02:27 -0800

    Update stdrand.h

--- engine/ml/train/stdrand.h
+++ engine/ml/train/stdrand.h
@@ -2,12 +2,29 @@
 #define STDRANDHH
 
 #include <cstdlib>
+#include <vector>
 
 struct stdrand{
 
   static float rand(){
     return (float)(std::rand()) / (float)(RAND_MAX);
   }
+
+  static std::vector<float> poormandirichlet(int n, float a){
+    std::vector<float> ans;
+    float tot = 0.0;
+    for(int ii=0; ii<n; ii++){
+      float v = 1.0*std::rand();
+      ans.push_back(v);
+      tot += v;
+    }
+
+    for(int ii=0; ii<nl ii++){
+      ans[ii] = ans[ii]/tot;
+    }
+    return ans;
+  }
+
 };
 
 #endif
