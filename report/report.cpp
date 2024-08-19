6b87012004a248582b7f467f375cc93890eeb2ce
stackasteak <146687315+stackasteak@users.noreply.github.com>
2024-08-18 19:52:30 -0700

    Update report.cpp

--- report/report.cpp
+++ report/report.cpp
@@ -6,6 +6,14 @@ int main(int argc, char** argv){
   lookrollout rro(5);
   rT rs(9,9,rro);
 
+  Game gg;
+    gg.reset();
+    for(int imove=0; imove< ds1->movenum; imove++){
+      gg.makemove(ds1->history[imove]);
+    }
+
+    rs1.run(gg, ds1->nx, ds1->ny);
+
   return 0;
 }
 
